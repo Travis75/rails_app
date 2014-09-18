dd0a9a597f52cafbf5c28ad235a406f5a7bd5f11
Travis75 <travis.gibson75@gmail.com>
2014-09-17 21:46:51 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HartlProjectOne::Application.config.secret_key_base = '27cb4a8ef8d28ae6f48b6833b0f73beb2fb78cbe67fd69db680d4b3dbb2f8a442c4818b598acae7257183fda28cc396e7283d3bf816299477578de05ffa2ad62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HartlProjectOne::Application.config.session_store :cookie_store, key: '_hartl_project_one_session'
